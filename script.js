2494924be34bea7075be9b08929ce90a651f29c0
devkatharine <devkatharine@gmail.com>
2022-08-24 11:32:38 -0300

    update modal added names

--- script.js
+++ script.js
@@ -4,20 +4,28 @@ const cs = el => document.querySelectorAll(el)
 pizzaJson.map((item, index) => {
   let pizzaItem = c('.models .pizza-item').cloneNode(true)
 
+
+  pizzaItem.setAttribute('data-key', index);
+
+
+
   pizzaItem.querySelector('.pizza-item--img img').src = item.img
-  pizzaItem.querySelector(
-    '.pizza-item--price'
-  ).innerHTML = `R$ ${item.price.toFixed(2)}`
+  pizzaItem.querySelector( '.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
   pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
   pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
   pizzaItem.querySelector('a').addEventListener('click', e => {
     e.preventDefault()
 
-    c('.pizzaWindowArea').style.opacity = 0;
-    c('.pizzaWindowArea').style.display = 'flex';
+    let key = e.target.closest('.pizza-item').getAttribute('data-key');
 
-    setTimeout(()=>{c('.pizzaWindowArea').style.opacity = 1;}, 200);
+    c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
 
+    c('.pizzaWindowArea').style.opacity = 0
+    c('.pizzaWindowArea').style.display = 'flex'
+
+    setTimeout(() => {
+      c('.pizzaWindowArea').style.opacity = 1
+    }, 200)
   })
 
   c('.pizza-area').append(pizzaItem)
