9a45a0c81e1b1847f8f102e17f559af0348af6f0
devkatharine <devkatharine@gmail.com>
2022-08-29 15:01:45 -0300

    update modal

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-let modalQtc = 1;
-
-
-
+let modalQt = 1
 
 const c = el => document.querySelector(el)
 const cs = el => document.querySelectorAll(el)
@@ -9,36 +6,38 @@ const cs = el => document.querySelectorAll(el)
 pizzaJson.map((item, index) => {
   let pizzaItem = c('.models .pizza-item').cloneNode(true)
 
+  //LISTAGEM DAS PIZZAS
 
-  pizzaItem.setAttribute('data-key', index);
+  pizzaItem.setAttribute('data-key', index)
 
-
-
-  pizzaItem.querySelector('.pizza-item--img img').src = item.img;
-  pizzaItem.querySelector( '.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
-  pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
-  pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+  pizzaItem.querySelector('.pizza-item--img img').src = item.img
+  pizzaItem.querySelector(
+    '.pizza-item--price'
+  ).innerHTML = `R$ ${item.price.toFixed(2)}`
+  pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
+  pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
   pizzaItem.querySelector('a').addEventListener('click', e => {
     e.preventDefault()
 
-    let key = e.target.closest('.pizza-item').getAttribute('data-key');
+    let key = e.target.closest('.pizza-item').getAttribute('data-key')
 
-    c('.pizzaBig img').src = pizzaJson[key].img;
+    c('.pizzaBig img').src = pizzaJson[key].img
 
-    c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
-    c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
-    c('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
-    c('.pizzaInfo--size.selected').classList.remove('selected');
-    cs('.pizzaInfo--size').forEach((size, sizeIndex)=>{
-      if(sizeIndex == 2) {
-        size.classList.add('selected');
+    c('.pizzaInfo h1').innerHTML = pizzaJson[key].name
+    c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description
+    c('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(
+      2
+    )}`
+    c('.pizzaInfo--size.selected').classList.remove('selected')
+    cs('.pizzaInfo--size').forEach((size, sizeIndex) => {
+      if (sizeIndex == 2) {
+        size.classList.add('selected')
       }
 
+      size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex]
+    })
 
-      size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
-    });
-
-    c('.pizzaInfo--qt').innerHTML = modalQt;
+    c('.pizzaInfo--qt').innerHTML = modalQt
 
     c('.pizzaWindowArea').style.opacity = 0
     c('.pizzaWindowArea').style.display = 'flex'
@@ -48,5 +47,11 @@ pizzaJson.map((item, index) => {
     }, 200)
   })
 
-  c('.pizza-area').append(pizzaItem);
+  c('.pizza-area').append(pizzaItem)
 })
+
+//EVENTOS DO MODAL
+
+function closeModal() {
+  
+}
