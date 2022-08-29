a3863f2f740587e00a339ec5149531968fbd89d8
devkatharine <devkatharine@gmail.com>
2022-08-29 16:31:54 -0300

    added pizza size

--- script.js
+++ script.js
@@ -81,3 +81,12 @@ c('.pizzaInfo--qtmais').addEventListener('click', ()=>{
   c('.pizzaInfo--qt').innerHTML = modalQt;
 
 });
+
+cs('.pizzaInfo--size').forEach((size, sizeIndex)=>{
+  size.addEventListener('click', (e)=>{
+
+    c('.pizzaInfo--size.selected').classList.remove('selected');
+    size.classList.add('selected');
+
+  });
+})
